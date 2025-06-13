80374c65491615b6cec33d1232ad8322ea30a756
vijayagopalsb <vijayagopal.sb@gmail.com>
2025-06-13 16:08:16 +0530

    Eureka Server App Update.

--- eureke-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eureke-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -3,12 +3,21 @@ package com.example.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(EurekaServerApplication.class);
     public static void main(String[] args) {
+    	
+    	LOGGER.info("Starting Eureka Server...");
         SpringApplication.run(EurekaServerApplication.class, args);
+        LOGGER.info("Eureka Server Started Successfully.");
+        
     }
 }
 
