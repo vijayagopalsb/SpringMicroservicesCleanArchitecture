8d431f328029b6c3576ee7a90e381b0062851340
vijayagopalsb <vijayagopal.sb@gmail.com>
2025-06-13 15:55:37 +0530

    Start add logger to source code

--- order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
@@ -2,10 +2,19 @@ package com.example.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class OrderServiceApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(OrderServiceApplication.class, args);
-    }
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(OrderServiceApplication.class);
+
+	public static void main(String[] args) {
+		
+		LOGGER.info("Starting Eureke Server...");
+		SpringApplication.run(OrderServiceApplication.class, args);
+		LOGGER.info("Eureka Server Started Successfully.");
+		
+	}
 }
--- user-service/src/main/java/com/example/userservice/OrderServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/OrderServiceApplication.java
@@ -1,10 +1,18 @@
 package com.example.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class OrderServiceApplication {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(OrderServiceApplication.class);
     public static void main(String[] args) {
+    	
+    	LOGGER.info("Starting Order Service...");
         SpringApplication.run(OrderServiceApplication.class, args);
+        LOGGER.info("Order Service Started Successfully");
+        
     }
 }
